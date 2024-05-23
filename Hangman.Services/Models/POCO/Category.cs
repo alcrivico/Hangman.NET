140232267441adbc2f3657856f9828df0c1e75a8
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 22:49:36 -0600

    Agregado Category POCO

---
+++ Hangman.Services/Models/POCO/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Models.POCO
+{
+    [Table (Name = "Category")]
+    public class Category
+    {
+        [Column (Name = "IdCategory", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdCategory { get; set; }
+
+        [Column (Name = "CategoryES")]
+        public string CategoryES { get; set; }
+
+        [Column (Name = "CategoryEN")]
+        public string CategoryEN { get; set; }
+    }
+}
