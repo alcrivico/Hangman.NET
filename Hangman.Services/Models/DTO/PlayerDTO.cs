21fee2960c33d96bc95623c75db29cbd4fcbaa0f
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 23:22:30 -0600

    Agregado método LogIn

--- Hangman.Services/Models/DTO/PlayerDTO.cs
+++ Hangman.Services/Models/DTO/PlayerDTO.cs
@@ -1,7 +1,9 @@
-﻿using Hangman.Services.Utilities;
+﻿using Hangman.Services.Models.POCO;
+using Hangman.Services.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Data.Linq;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -17,12 +19,40 @@ namespace Hangman.Services.Models.DTO
 
             if (data != null)
             {
+                try
+                {
+                    Table<Player> playerTable = data.GetTable<Player>();
 
+                    var query = from player in playerTable
+                                where player.Email == email && player.Password == password
+                                select player;
+
+                    if (query.Any())
+                    {
+                        response["Error"] = false;
+                        response.Add("Message", "Inicio de sesión exitoso");
+                        response.Add("Player", query.First());
+                    }
+                    else
+                    {
+                        response.Add("Message", "Correo o contraseña incorrectos. Por favor, verifíquelos");
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine(sqlEx.Message);
+                }
+                finally
+                {
+                    data.Dispose();
+                }
             }
             else
             {
-                response.Add("Mensaje", "Correo y/o contraseña incorrectos. Por favor, verifíquelos");
+                response.Add("Message", Constants.ERROR_CONNECTION_MESSAGE);
             }
+
+            return response;
         }
     }
 }
