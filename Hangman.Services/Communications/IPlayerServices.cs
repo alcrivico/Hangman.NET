a349d89016e0e6cafe42cd0143136e1de7894347
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 23:33:06 -0600

    Agregado servicio LogIn

---
+++ Hangman.Services/Communications/IPlayerServices.cs
@@ -0,0 +1,18 @@
+﻿using Hangman.Services.Models.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Hangman.Services.Communications
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IPlayerServices" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IPlayerServices
+    {
+        [OperationContract]
+        Player LogIn(string email, string pass);//Cambiar el retorno
+    }
+}
---
+++ Hangman.Services/Communications/PlayerServices.svc.cs
@@ -0,0 +1,21 @@
+﻿using Hangman.Services.Models.DTO;
+using Hangman.Services.Models.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Hangman.Services.Communications
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "PlayerServices" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione PlayerServices.svc o PlayerServices.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class PlayerServices : IPlayerServices
+    {
+        public Player LogIn(string email, string pass)
+        {
+            return (Player)PlayerDTO.LogIn(email, pass)["Player"];
+        }
+    }
+}
