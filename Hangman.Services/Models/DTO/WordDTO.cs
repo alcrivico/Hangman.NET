7b4166b4278d64524063d99988bf39f170f06963
RaulHernandez23 <raulh230600@gmail.com>
2024-05-23 01:32:49 -0600

    Agregado método GetWordsList

--- Hangman.Services/Models/DTO/WordDTO.cs
+++ Hangman.Services/Models/DTO/WordDTO.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Hangman.Services.Models.POCO;
+using Hangman.Services.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +11,45 @@ namespace Hangman.Services.Models.DTO
 {
     public class WordDTO
     {
+        public static Dictionary<string, object> GetWordsList()
+        {
+            Dictionary<string, object> response = new Dictionary<string, object>
+            {
+                { "Error", true },
+                { "Message", "" }
+            };
+            DataContext data = DBConnection.GetConnection();
 
+            if (data != null)
+            {
+                try
+                {
+                    Table<Word> wordTable = data.GetTable<Word>();
+
+                    var query = from word in wordTable
+                                select word;
+                    if (query.Any())
+                    {
+                        response["Error"] = false;
+                        response["Message"] = "Palabras encontradas";
+                        response.Add("Words", query.ToList());
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.Write(sqlEx.StackTrace);
+                }
+                finally
+                {
+                    data.Dispose();
+                }
+            }
+            else
+            {
+                response["Message"] = Constants.ERROR_CONNECTION_MESSAGE;
+            }
+
+            return response;
+        }
     }
 }
