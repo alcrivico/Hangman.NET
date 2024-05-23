980a2f01637360f8a4be2fdb381642faf36c3a16
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-05-22 22:26:25 -0600

    Se agregó la capa de servicios con un archivo de conexión a la base de datos

---
+++ Hangman.Services/Utilities/DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Utilities
+{
+
+    public class DBConnection
+    {
+        
+        public static string connectionstring = "Data Source=PC\\SQLEXPRESS;Initial Catalog=Hangman.Data;Integrated Security=True;Encrypt=False";
+
+        public static DataContext GetConnection()
+        {
+            return new DataContext(connectionstring);
+        }
+
+    }
+
+}
