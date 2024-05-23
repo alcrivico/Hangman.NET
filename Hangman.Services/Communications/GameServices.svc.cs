264924075f42e5b86aa5ac29ad1e4d03e9747c5b
Victoria Moyano <Soyunpokemonytuno@hotmail.com>
2024-05-23 00:18:43 -0600

    Creacion del servicio de partida

---
+++ Hangman.Services/Communications/GameServices.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Hangman.Services.Communications
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "GameServices" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione GameServices.svc o GameServices.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class GameServices : IGameServices
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
