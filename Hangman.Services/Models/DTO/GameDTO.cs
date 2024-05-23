66bf26acfc05a96162f40d765e1347d5728387b6
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 23:15:52 -0600

    Agregado método LogIn

--- Hangman.Services/Models/DTO/GameDTO.cs
+++ Hangman.Services/Models/DTO/GameDTO.cs
@@ -9,20 +9,6 @@ namespace Hangman.Services.Models.DTO
 {
     public class GameDTO
     {
-        public static Dictionary<string, object> LogIn (string email, string password)
-        {
-            Dictionary <string, object> response = new Dictionary<string, object> ();
-            response.Add("Error", true);
-            DataContext data = DBConnection.GetConnection();
-
-            if (data != null)
-            {
-
-            }
-            else
-            {
-                response.Add("Mensaje", "Correo y/o contraseña incorrectos. Por favor, verifíquelos");
-            }
-        }
+        
     }
 }
--- Hangman.Services/Models/DTO/PlayerDTO.cs
+++ Hangman.Services/Models/DTO/PlayerDTO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Hangman.Services.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,20 @@ namespace Hangman.Services.Models.DTO
 {
     public class PlayerDTO
     {
+        public static Dictionary<string, object> LogIn(string email, string password)
+        {
+            Dictionary<string, object> response = new Dictionary<string, object>();
+            response.Add("Error", true);
+            DataContext data = DBConnection.GetConnection();
+
+            if (data != null)
+            {
+
+            }
+            else
+            {
+                response.Add("Mensaje", "Correo y/o contraseña incorrectos. Por favor, verifíquelos");
+            }
+        }
     }
 }
