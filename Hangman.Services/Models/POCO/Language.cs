b88ae0c213575e23a442ab9a120ee23ee628dc51
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 22:43:58 -0600

    Agregado Language POCO

---
+++ Hangman.Services/Models/POCO/Language.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Models.POCO
+{
+    [Table (Name = "Language")]
+    public class Language
+    {
+        [Column (Name = "IdLanguage", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdLanguage { get; set; }
+
+        [Column (Name = "LanguageName")]
+        public string LanguageName { get; set; }
+    }
+}
