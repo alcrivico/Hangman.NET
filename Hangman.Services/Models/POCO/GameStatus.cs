2fe0e28d482bf1efed64cd482f859cbb80556b58
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 22:42:36 -0600

    Agregado GameStatus en POCO

---
+++ Hangman.Services/Models/POCO/GameStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Models.POCO
+{
+    [Table (Name = "GameStatus")]
+    public class GameStatus
+    {
+        [Column (Name = "IdStatus", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdStatus { get; set; }
+
+        [Column (Name = "Status")]
+        public string Status { get; set; }
+    }
+}
