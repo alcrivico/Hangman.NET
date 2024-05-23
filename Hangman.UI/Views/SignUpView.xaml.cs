9b30ecc28bdb63b9f9cfc8f2de85f73f5d5ca79b
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-05-23 03:33:23 -0600

    Se agregaron los textfield de SignUpView

--- Hangman.UI/Views/SignUpView.xaml.cs
+++ Hangman.UI/Views/SignUpView.xaml.cs
@@ -23,5 +23,10 @@ namespace Hangman.UI.Views
         {
             InitializeComponent();
         }
+
+        private void TextBoxControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
