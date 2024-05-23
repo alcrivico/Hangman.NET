4a452810fca590e5b941a2bff1bdc0801bb8ad29
RaulHernandez23 <raulh230600@gmail.com>
2024-05-22 22:36:01 -0600

    Agregado Player POCO

---
+++ Hangman.Services/Models/POCO/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Models.POCO
+{
+    [Table (Name = "Player")]
+    public class Player
+    {
+        [Column (Name = "IdPlayer", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdPlayer { get; set; }
+
+        [Column (Name = "Name")]
+        public string Name { get; set; }
+
+        [Column (Name = "FirsLastName")]
+        public string FirsLastName { get; set; }
+
+        [Column (Name = "SecondLastName")]
+        public string SecondLastName { get; set; }
+
+        [Column (Name = "BirhtDate")]
+        public DateTime BirhtDate { get; set; }
+
+        [Column (Name = "Email")]
+        public string Email { get; set; }
+
+        [Column (Name = "Password")]
+        public string Password { get; set; }
+    }
+}
