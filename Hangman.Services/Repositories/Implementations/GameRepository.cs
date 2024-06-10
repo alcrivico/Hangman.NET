94d00ef888fc51e6a6380a9e26779749308e918e
RaulHernandez23 <raulh230600@gmail.com>
2024-06-09 20:11:27 -0600

    Corregido defecto en GameRepository

--- Hangman.Services/Repositories/Implementations/GameRepository.cs
+++ Hangman.Services/Repositories/Implementations/GameRepository.cs
@@ -21,7 +21,7 @@ namespace Hangman.Services.Repositories.Implementations
 
             Dictionary<string, object> response = new Dictionary<string, object>();
 
-            using (YourDataContext dataSource = new YourDataContext(DBConnection.ConnectionString))
+            using (YourDataContext dataSource = new YourDataContext(DBConnection.connectionString))
             {
 
                 if (dataSource != null)
