62520d97500e581b326426b896e78893cc02d526
RaulHernandez23 <raulh230600@gmail.com>
2024-06-10 01:22:00 -0600

    Agregado LeftGame

--- Hangman.Adapters/ControllerAdapters/SingleAdapters/GameAdapter.cs
+++ Hangman.Adapters/ControllerAdapters/SingleAdapters/GameAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hangman.Adapters.ControllerAdapters.Services.Game;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Hangman.Adapters.ControllerAdapters.SingleAdapters
 {
-    internal class GameAdapter
+    public class GameAdapter
     {
+        public int LeftGame(string gameCode)
+        {
+            var service = new GameServicesClient();
+
+            GameDTO response = service.SetGameStatusAsync(gameCode, "Left").Result;
+
+            switch (response.ResponseCode)
+            {
+                case 1:
+                    throw new Exception("No se encontró información de la entidad en la base de datos");
+                case 2:
+                    throw new Exception("Error en la consulta, verifique el error en el servidor");
+                case 3:
+                    throw new Exception("No se ha podido conectar con la base de datos. Por favor, intente más tarde");
+
+            }
+
+            return response.ResponseCode;
+        }
     }
 }
