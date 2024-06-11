70a50b95679038403f6ea2140024107b4119cbcc
RaulHernandez23 <raulh230600@gmail.com>
2024-06-11 02:18:03 -0600

    Ponido DBConnecction

---
+++ Hangman.Services/Utilities/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Services.Utilities
+{
+    public class DBConnection
+    {
+        public static string connectionString = "Data Source=hangman-uv.database.windows.net;Initial Catalog=Hangman.data;Persist Security Info=True;User ID=hangman;Password=soyunpockemonytuno123#";
+
+        public static DataContext GetConnection()
+        {
+            return new DataContext(connectionString);
+        }
+    }
+}
