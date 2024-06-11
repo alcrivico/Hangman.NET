27fce70f942d00041ac19fd631934b47ef0e8583
Victoria Moyano <Soyunpokemonytuno@hotmail.com>
2024-06-10 18:16:40 -0600

    Creacion del CreateGameAdapter

---
+++ Hangman.Adapters/ControllerAdapters/SingleAdapters/CreateGameAdapter.cs
@@ -0,0 +1,36 @@
+﻿using Hangman.Adapters.ControllerAdapters.Services.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hangman.Adapters.ControllerAdapters.SingleAdapters
+{
+    internal class CreateGameAdapter
+    {
+        public void CreateGame(GameDTO newGame)
+        {
+            var service = new GameServicesClient();
+
+            GameDTO response = service.CreateGameAsync(newGame).Result;
+
+            switch (response.ResponseCode)
+            {
+                case 0:
+                    Console.WriteLine("La partida se creó correctamente.");
+                    break;
+                case 1:
+                    throw new Exception("No se encontró información de la entidad en la base de datos");
+                case 2:
+                    throw new Exception("Error en la consulta, verifique el error en el servidor");
+                case 3:
+                    throw new Exception("No se ha podido conectar con la base de datos. Por favor, intente más tarde");
+                default:
+                    throw new Exception("Se recibió un código de respuesta inesperado: " + response.ResponseCode);
+            }
+
+        }
+    }
+}
