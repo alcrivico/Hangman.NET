de7e0458cc0803b06e07c77d7c64ab219a017331
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-06-15 08:10:28 -0600

    Se implementó la utilidad de los sockets en GameView

--- Hangman.UI/Views/GameView.xaml.cs
+++ Hangman.UI/Views/GameView.xaml.cs
@@ -5,7 +5,9 @@ using Hangman.Adapters.ControllerAdapters.SingleAdapters;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -31,11 +33,83 @@ namespace Hangman.UI.Views
         PlayerDTO _playerDTO;
         WordDTO _word;
         int _wrongLetters;
-        ClientSocket _clientSocket = new ClientSocket();
+        TcpClient _challengerClient;
+        NetworkStream _challengerStream;
+        TcpClient _creatorClient;
+        NetworkStream _creatorStream;
 
         public GameView()
         {
             InitializeComponent();
+        }
+
+        private void StartChallengerClient()
+        {
+
+            _challengerClient = new TcpClient("127.0.0.1", 5000);
+            _challengerStream = _challengerClient.GetStream();
+            
+        }
+
+        private void StartCreatorClient()
+        {
+
+            _creatorClient = new TcpClient("127.0.0.1", 5000);
+            _creatorStream = _creatorClient.GetStream();
+
+        }
+
+        private void SendLetter(string letter)
+        {
+
+            byte[] data = Encoding.UTF8.GetBytes(letter);
+
+            _challengerStream.Write(data, 0, data.Length);
+
+        }
+
+        private void ListenForLetters()
+        {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = _creatorStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+
+                string letter = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                if (letter == "Game Over")
+                {
+
+                    if (_gameDTO.Language == "Spanish")
+                    {
+                        MessageBox.Show("Fin del Juego", _word.WordES, MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Game Over", _word.WordEN, MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+
+                    MenuView menuView = new MenuView(_playerDTO);
+
+                    menuView.Show();
+
+                    this.Close();
+
+                }
+                else
+                {
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        ReceiveLetter(letter);
+                    });
+
+                }
+
+            }
+
         }
 
         public GameView(Adapters.ControllerAdapters.Services.Game.GameDTO gameDTO, PlayerDTO playerDTO)
@@ -52,6 +126,43 @@ namespace Hangman.UI.Views
 
             InitializeComponent();
 
+            try
+            {
+
+                _word = _gameAdapter.SearchWord(_gameDTO.WordEN);
+
+                _categories.Add(_word.CategoryES);
+                _categories.Add(_word.CategoryEN);
+                _tips.Add(_word.TipES);
+                _tips.Add(_word.TipEN);
+
+                if (_gameDTO.Language == "Spanish")
+                {
+
+                    Information_Category.GameInformation = "Categoría: " + _categories[0];
+                    Information_Tip.GameInformation = "Tip: " + _tips[0];
+                    InformationMessage.AlertMessageText = "Creador de la partida: " + _gameDTO.CreatorName + "\nCódigo de Partida: " + _gameDTO.GameCode;
+                    HangmanWord.Word = _word.WordES;
+                }
+                else
+                {
+
+                    Information_Category.GameInformation = "Category: " + _categories[1];
+                    Information_Tip.GameInformation = "Tip: " + _tips[1];
+                    InformationMessage.AlertMessageText = "Game Creator: " + _gameDTO.CreatorName + "\nGame Code: " + _gameDTO.GameCode;
+                    HangmanWord.Word = _word.WordEN;
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+
+            }
+
             if (_gameDTO.CreatorName != _playerDTO.Name)
             {
 
@@ -60,31 +171,6 @@ namespace Hangman.UI.Views
 
                     _gameAdapter.SetChallenger(_gameDTO.GameCode, _playerDTO.Email);
                     _gameAdapter.SetGameStatus(_gameDTO.GameCode, "Playing");
-
-                    _word = _gameAdapter.SearchWord(_gameDTO.WordEN);
-
-                    _categories.Add(_word.CategoryES);
-                    _categories.Add(_word.CategoryEN);
-                    _tips.Add(_word.TipES);
-                    _tips.Add(_word.TipEN);
-
-                    if(_gameDTO.Language == "Spanish") 
-                    {
-
-                        Information_Category.GameInformation = "Categoría: " + _categories[0];
-                        Information_Tip.GameInformation = "Tip: " + _tips[0];
-                        InformationMessage.AlertMessageText = "Creador de la partida: " + _gameDTO.CreatorName  + "\nCódigo de Partida: " + _gameDTO.GameCode;
-                        HangmanWord.Word = _word.WordES;
-                    }
-                    else
-                    {
-
-                        Information_Category.GameInformation = "Category: " + _categories[1];
-                        Information_Tip.GameInformation = "Tip: " + _tips[1];
-                        InformationMessage.AlertMessageText = "Game Creator: " + _gameDTO.CreatorName + "\nGame Code: " + _gameDTO.GameCode;
-                        HangmanWord.Word = _word.WordEN;
-
-                    }
 
                 }
                 catch (Exception e)
@@ -95,29 +181,21 @@ namespace Hangman.UI.Views
 
                 }
 
+                Task.Run(() => StartChallengerClient());
+
             }
             else
             {
-                Keyboard.Visibility = Visibility.Hidden;
-            }
-            _clientSocket.StartClientSocket(_gameDTO.GameCode);
-            Task.Run(() => ReceiveMessagesLoop());
-        }
 
-        private async Task ReceiveMessagesLoop()
-        {
-            while (true)
-            {
-                string message = await _clientSocket.ReceiveMessage();
-                if (message != null)
-                {
-                    Dispatcher.Invoke(() =>
-                    {
-                        //Aqui meter lo que pasa cuando se recibe el mensaje
-                        MessageBox.Show("El mensaje recibido es: " + message);
-                    });
-                }
+                Keyboard.Visibility = Visibility.Hidden;
+
+                Task.Run(() => StartCreatorClient());
+
+                //Traer Ventana de Espera por encima de la ventana de juego
+
+
             }
+
         }
 
         private void TitleBarControl_WindowStateChangeRequested(object sender, WindowState e)
@@ -142,7 +220,7 @@ namespace Hangman.UI.Views
 
         private void ReceiveLetter(String letter)
         {
-            //_clientSocket.SendMessage(letter);
+            
             if (_gameDTO.CreatorName != _playerDTO.Name)
             {
 
@@ -151,6 +229,7 @@ namespace Hangman.UI.Views
 
                     if (_gameDTO.Language == "Spanish")
                     {
+
 
                         if (_word.WordES.ToUpper().Contains(letter))
                         {
@@ -164,6 +243,8 @@ namespace Hangman.UI.Views
                                 _gameAdapter.SetGameStatus(_gameDTO.GameCode, "Won");
 
                                 MessageBox.Show("¡Felicidades! Has ganado la partida", _word.WordES, MessageBoxButton.OK, MessageBoxImage.Information);
+
+                                SendLetter("Game Over");
                                 
                                 MenuView menuView = new MenuView(_playerDTO);
 
@@ -225,6 +306,8 @@ namespace Hangman.UI.Views
 
                                 MessageBox.Show("Congratulations! You have won the game", _word.WordEN, MessageBoxButton.OK, MessageBoxImage.Information);
 
+                                SendLetter("Game Over");
+
                                 MenuView menuView = new MenuView(_playerDTO);
 
                                 menuView.Show();
@@ -277,193 +360,285 @@ namespace Hangman.UI.Views
                 }
 
             }
+            else
+            {
+
+                try
+                {
+
+                    if (_gameDTO.Language == "Spanish")
+                    {
+
+                        if (_word.WordES.ToUpper().Contains(letter))
+                        {
+                            HangmanWord.DiscoverLetter(letter);
+                        }
+                        else
+                        {
+
+                            _wrongLetters++;
+                            Hangman.IncorrectGuesses = _wrongLetters;
+
+                        }
+
+                    }
+                    else if (_gameDTO.Language == "English")
+                    {
+
+                        if (_word.WordEN.ToUpper().Contains(letter))
+                        {
+                            HangmanWord.DiscoverLetter(letter);
+                        }
+                        else
+                        {
+
+                            _wrongLetters++;
+                            Hangman.IncorrectGuesses = _wrongLetters;
+
+
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    _gameAdapter.SetGameStatus(_gameDTO.GameCode, "Cancelled");
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    MenuView menuView = new MenuView(_playerDTO);
+
+                    menuView.Show();
+
+                    this.Close();
+
+                }
+
+            }
 
         }
 
         private void Keyboard_Btn0Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("0");
+            SendLetter("0");
 
         }
 
         private void Keyboard_Btn1Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("1");
+            SendLetter("1");
         }
 
         private void Keyboard_Btn2Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("2");
+            SendLetter("2");
         }
 
         private void Keyboard_Btn3Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("3");
+            SendLetter("3");
         }
 
         private void Keyboard_Btn4Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("4");
+            SendLetter("4");
         }
 
         private void Keyboard_Btn5Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("5");
+            SendLetter("5");
         }
 
         private void Keyboard_Btn6Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("6");
+            SendLetter("6");
         }
 
         private void Keyboard_Btn7Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("7");
+            SendLetter("7");
         }
 
         private void Keyboard_Btn8Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("8");
+            SendLetter("8");
         }
 
         private void Keyboard_Btn9Click(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("9");
+            SendLetter("9");
         }
 
         private void Keyboard_BtnAClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("A");
+            SendLetter("A");
         }
 
         private void Keyboard_BtnBClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("B");
+            SendLetter("B");
         }
 
         private void Keyboard_BtnCClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("C");
+            SendLetter("C");
         }
 
         private void Keyboard_BtnDClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("D");
+            SendLetter("D");
         }
 
         private void Keyboard_BtnEClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("E");
+            SendLetter("E");
         }
 
         private void Keyboard_BtnFClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("F");
+            SendLetter("F");
         }
 
         private void Keyboard_BtnGClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("G");
+            SendLetter("G");
         }
 
         private void Keyboard_BtnHClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("H");
+            SendLetter("H");
         }
 
         private void Keyboard_BtnIClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("I");
+            SendLetter("I");
         }
 
         private void Keyboard_BtnJClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("J");
+            SendLetter("J");
         }
 
         private void Keyboard_BtnKClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("K");
+            SendLetter("K");
         }
 
         private void Keyboard_BtnLClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("L");
+            SendLetter("L");
         }
 
         private void Keyboard_BtnMClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("M");
+            SendLetter("M");
         }
 
         private void Keyboard_BtnNClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("N");
+            SendLetter("N");
         }
 
         private void Keyboard_BtnÑClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("Ñ");
+            SendLetter("Ñ");
         }
 
         private void Keyboard_BtnOClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("O");
+            SendLetter("O");
         }
 
         private void Keyboard_BtnPClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("P");
+            SendLetter("P");
         }
 
         private void Keyboard_BtnQClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("Q");
+            SendLetter("Q");
         }
 
         private void Keyboard_BtnRClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("R");
+            SendLetter("R");
         }
 
         private void Keyboard_BtnSClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("S");
+            SendLetter("S");
         }
 
         private void Keyboard_BtnTClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("T");
+            SendLetter("T");
         }
 
         private void Keyboard_BtnUClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("U");
+            SendLetter("U");
         }
 
         private void Keyboard_BtnVClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("V");
+            SendLetter("V");
         }
 
         private void Keyboard_BtnWClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("W");
+            SendLetter("W");
         }
 
         private void Keyboard_BtnXClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("X");
+            SendLetter("X");
         }
 
         private void Keyboard_BtnYClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("Y");
+            SendLetter("Y");
         }
 
         private void Keyboard_BtnZClick(object sender, RoutedEventArgs e)
         {
             ReceiveLetter("Z");
+            SendLetter("Z");
         }
 
     }
