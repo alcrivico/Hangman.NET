8659af77b2f1408f7aca4d4fb2fc49f94156e08b
Albhieri Cristoff Villa Contreras <alcrivico@gmail.com>
2024-06-21 15:37:55 -0600

    Se arregló el Query de la base de datos para que se reconstruya sin eliminar la base de datos manualmente

--- Database/HangmanTables.sql
+++ Database/HangmanTables.sql
@@ -1,36 +1,42 @@
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[Category]') AND type IN ('U'))
-	DROP TABLE [dbo].[Category]
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Hangman.Data')
+BEGIN
+    CREATE DATABASE [Hangman.Data];
+END
+GO
+
+USE [Hangman.Data];
+GO
+
+IF OBJECT_ID('Game', 'U') IS NOT NULL DROP TABLE [Game];
+IF OBJECT_ID('Word', 'U') IS NOT NULL DROP TABLE [Word];
+IF OBJECT_ID('Player', 'U') IS NOT NULL DROP TABLE [Player];
+IF OBJECT_ID('Language', 'U') IS NOT NULL DROP TABLE [Language];
+IF OBJECT_ID('GameStatus', 'U') IS NOT NULL DROP TABLE [GameStatus];
+IF OBJECT_ID('Category', 'U') IS NOT NULL DROP TABLE [Category];
+GO
 
 CREATE TABLE [Category] (
-    [Id] int IDENTITY(1,1) NOT NULL,
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [CategoryES] varchar(55) NOT NULL,
     [CategoryEN] varchar(55) NOT NULL
-)
-
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[GameStatus]') AND type IN ('U'))
-	DROP TABLE [dbo].[GameStatus]
+);
+GO
 
 CREATE TABLE [GameStatus] (
-    -- Waiting, Playing, Won, Lost, Cancelled, Left
-    -- Esperando, Jugando, Ganado, Perdido, Cancelado, Abandonado
-    [Id] int IDENTITY(1,1) NOT NULL,
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [StatusEn] varchar(10) NULL,
     [StatusEs] varchar(10) NULL
-)
-
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[Language]') AND type IN ('U'))
-	DROP TABLE [dbo].[Language]
+);
+GO
 
 CREATE TABLE [Language] (
-    [Id] int  IDENTITY(1,1) NOT NULL,
-    [LanguageName] varchar(55)  NULL
-)
-
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[Player]') AND type IN ('U'))
-	DROP TABLE [dbo].[Player]
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [LanguageName] varchar(55) NULL
+);
+GO
 
 CREATE TABLE [Player] (
-    [Id] int IDENTITY(1,1) NOT NULL,
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [FirstName] varchar(55) NOT NULL,
     [FirstLastName] varchar(55) NOT NULL,
     [SecondLastName] varchar(55) NULL,
@@ -38,45 +44,34 @@ CREATE TABLE [Player] (
     [Email] varchar(55) NOT NULL,
     [Password] varchar(55) NOT NULL,
     [Telephone] varchar(10) NOT NULL
-)
-
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[Word]') AND type IN ('U'))
-	DROP TABLE [dbo].[Word]
+);
+GO
 
 CREATE TABLE [Word] (
-    [Id] int IDENTITY(1,1) NOT NULL,
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [WordES] varchar(55) NOT NULL,
     [WordEN] varchar(55) NOT NULL,
     [TipES] varchar(55) NOT NULL,
     [TipEN] varchar(55) NOT NULL,
     [HasNumber] bit NOT NULL,
-    [CategoryId] int NOT NULL
-)
-
-IF EXISTS (SELECT * FROM sys.all_objects WHERE object_id = OBJECT_ID(N'[dbo].[Game]') AND type IN ('U'))
-	DROP TABLE [dbo].[Game]
+    [CategoryId] int NOT NULL,
+    CONSTRAINT FK_Word_Category FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+);
+GO
 
 CREATE TABLE [Game] (
-    [Id] int IDENTITY(1,1) NOT NULL,
+    [Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
     [CreationDate] datetime NOT NULL,
     [GameCode] varchar(14) NOT NULL,
     [StatusId] int NOT NULL,
     [CreatorId] int NOT NULL,
     [ChallengerId] int NULL,
     [WordId] int NOT NULL,
-    [LanguageId] int NOT NULL
-)
-
-ALTER TABLE [Category] ADD CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
-ALTER TABLE [GameStatus] ADD CONSTRAINT [PK_GameStatus] PRIMARY KEY ([Id])
-ALTER TABLE [Language] ADD CONSTRAINT [PK_Language] PRIMARY KEY ([Id])
-ALTER TABLE [Player] ADD CONSTRAINT [PK_Player] PRIMARY KEY ([Id])
-ALTER TABLE [Word] ADD CONSTRAINT [PK_Word] PRIMARY KEY ([Id])
-ALTER TABLE [Game] ADD CONSTRAINT [PK_Game] PRIMARY KEY ([Id])
-
-ALTER TABLE [Game] ADD CONSTRAINT [FK_Game_GameStatus] FOREIGN KEY ([StatusId]) REFERENCES [GameStatus] ([Id])
-ALTER TABLE [Game] ADD CONSTRAINT [FK_Game_Player_Creator] FOREIGN KEY ([CreatorId]) REFERENCES [Player] ([Id])
-ALTER TABLE [Game] ADD CONSTRAINT [FK_Game_Player_Challenger] FOREIGN KEY ([ChallengerId]) REFERENCES [Player] ([Id])
-ALTER TABLE [Game] ADD CONSTRAINT [FK_Game_Word] FOREIGN KEY ([WordId]) REFERENCES [Word] ([Id])
-ALTER TABLE [Game] ADD CONSTRAINT [FK_Game_Language] FOREIGN KEY ([LanguageId]) REFERENCES [Language] ([Id])
-ALTER TABLE [Word] ADD CONSTRAINT [FK_Word_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+    [LanguageId] int NOT NULL,
+    CONSTRAINT FK_Game_GameStatus FOREIGN KEY ([StatusId]) REFERENCES [GameStatus] ([Id]),
+    CONSTRAINT FK_Game_Player_Creator FOREIGN KEY ([CreatorId]) REFERENCES [Player] ([Id]),
+    CONSTRAINT FK_Game_Player_Challenger FOREIGN KEY ([ChallengerId]) REFERENCES [Player] ([Id]),
+    CONSTRAINT FK_Game_Word FOREIGN KEY ([WordId]) REFERENCES [Word] ([Id]),
+    CONSTRAINT FK_Game_Language FOREIGN KEY ([LanguageId]) REFERENCES [Language] ([Id])
+);
+GO
